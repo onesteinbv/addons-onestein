1d0e2aa0ceb1461d125e627bac11f0368672d836
astirpe <a.stirpe@onestein.nl>
2016-03-02 16:58:02 +0100

    Update module mass_mailing_no_unsubscribe

--- mass_mailing_no_unsubscribe/__openerp__.py
+++ mass_mailing_no_unsubscribe/__openerp__.py
@@ -1,47 +1,24 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 ONESTEiN BV (<http://www.onestein.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Mass mailing unsubscription',
     'images': ['static/description/main_screenshot.png'],
     'summary': """Configurable unsubscribe possibility for mass mailing""",
-    'description': """
-Mass mailing unsubscribe
-========================
-This module lets you configure whether you want to have the standard unsubscribe link or
-add your own link to the mass mailing emails.
-""",
-    'version': '1.1',
+    'version': '8.0.1.0.0',
     'author': 'ONESTEiN BV',
+    'license': 'AGPL-3',
     'website': 'http://onestein.eu',
-    'category': 'Extra Tools',
+    'category': 'Marketing',
     'depends': [
         'mass_mailing'
     ],
     'data': [
-        'mail_view.xml'
+        'views/mail_mass_mailing.xml'
     ],
-    'qweb': [
-    ],
-    'demo': [
-    ],
+    'demo': [],
     'installable': True,
     'auto_install': False,
+    'application': False,
 }
--- mass_mailing_no_unsubscribe/models/__init__.py
+++ mass_mailing_no_unsubscribe/models/__init__.py
@@ -2,4 +2,5 @@
 # © 2015 ONESTEiN BV (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import mail_mail
+from . import mail_mass_mailing
