4ff667b972d553d0e4236fedad419297ab8dac1b
Andrea <a.stirpe@onestein.nl>
2016-09-09 15:32:43 +0200

    Module account_invoice_line_pricelist ported to V10

---
+++ account_invoice_line_pricelist/models/account_invoice.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    pricelist_id = fields.Many2one(
+        'product.pricelist',
+        'Pricelist',
+        help="Pricelist for current invoice.")
+
+    @api.onchange('partner_id', 'company_id')
+    def _onchange_partner_id(self):
+        res = super(AccountInvoice, self)._onchange_partner_id()
+        if self.type in ['out_invoice', 'out_refund']:
+            self.pricelist_id = None
+            if self.partner_id:
+                self.pricelist_id = self.partner_id.property_product_pricelist
+        return res
---
+++ account_invoice_line_pricelist/models/account_invoice_line.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        res = super(AccountInvoiceLine, self)._onchange_product_id()
+
+        if self.invoice_id.type not in ['out_invoice', 'out_refund']:
+            return res
+
+        partner = self.invoice_id.partner_id
+        pricelist = self.invoice_id.pricelist_id
+        product = self.product_id
+
+        if not partner or not product or not pricelist:
+            return res
+
+        product = product.with_context(
+            lang=partner.lang,
+            partner=partner.id,
+            quantity=self.quantity,
+            date=self.invoice_id.date_invoice,
+            pricelist=pricelist.id,
+            uom=self.uom_id.id
+        )
+
+        self.price_unit = self.env['account.tax']._fix_tax_included_price(
+            product.price,
+            product.taxes_id,
+            self.invoice_line_tax_ids)
+
+        return res
