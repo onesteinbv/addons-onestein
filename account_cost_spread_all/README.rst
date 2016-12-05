8406a958fd8d67c1430c918c3c7865099af2e7ab
Andrea <a.stirpe@onestein.nl>
2016-12-05 15:17:52 +0100

    Add module account_cost_spread_all

---
+++ account_cost_spread_all/README.rst
@@ -0,0 +1,22 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=============================================
+Cost / Income Spread - All moves in one click
+=============================================
+
+
+This is an extension of the module account_cost_spread;
+by installing this module, you can create all the moves
+for Cost and Income spreading in just one click.
+
+
+Credits
+=======
+
+
+Contributors
+------------
+
+* Andrea Stirpe <a.stirpe@onestein.nl>
---
+++ account_cost_spread_all/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Cost / Income Spread - All moves in one click',
+    'summary': """Create all the moves for Cost and Income spreading in just one click""",
+    'version': '1.0',
+    'author': "ONESTEiN BV",
+    'website': 'http://www.onestein.eu',
+    'category': 'Accounting',
+    'depends': [
+        'account_cost_spread',
+    ],
+    'data': [
+        'views/account_invoice_line_view.xml',
+    ],
+}
