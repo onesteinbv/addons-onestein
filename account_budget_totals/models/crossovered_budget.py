e6384fdada9bde1f98117d90b35583b76da840a5
Andrea <a.stirpe@onestein.nl>
2016-12-07 13:06:08 +0100

    flake8 for account_budget_totals

--- account_budget_totals/models/crossovered_budget.py
+++ account_budget_totals/models/crossovered_budget.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 import odoo.addons.decimal_precision as dp
 
 
