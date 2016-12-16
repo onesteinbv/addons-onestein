bbae418c26442a9286a8f75f461bd9e2fbf63f5c
Andrea <a.stirpe@onestein.nl>
2016-12-16 09:39:46 +0100

    Updated imports in account_invoice_line_pricelist

--- account_invoice_line_pricelist/models/account_invoice_line.py
+++ account_invoice_line_pricelist/models/account_invoice_line.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class AccountInvoiceLine(models.Model):
