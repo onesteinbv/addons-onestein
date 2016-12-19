bc4729a12ed4f5a8c614f70f6803c37addb4ab9a
Andrea <a.stirpe@onestein.nl>
2016-12-19 13:47:37 +0100

    Module project_issue_stage ported to V10

---
+++ project_issue_stage/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Project Issue Stage',
+    'summary': '''Configure the stages of the issues''',
+    'author': 'ONESTEiN BV',
+    'website': 'http://www.onestein.eu',
+    'category': 'Project Management',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'project',
+        'project_issue',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/project_project.xml',
+        'views/project_issue.xml',
+        'views/project_issue_stage.xml',
+        'menu_items.xml',
+    ],
+}
