e4660fa7ceb07faf5ffe525d06bbfb6e705c5382
Andrea <a.stirpe@onestein.nl>
2016-12-19 13:58:51 +0100

    Updated module project_issue_stage

--- project_issue_stage/models/__init__.py
+++ project_issue_stage/models/__init__.py
@@ -2,6 +2,6 @@
 # Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import project_issue_stage
 from . import project_project
 from . import project_issue
-from . import project_issue_stage
