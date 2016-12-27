fac7e94b51b7b56fd89011517ee91a6a91bbd8e0
Andrea <a.stirpe@onestein.nl>
2016-12-27 11:57:55 +0100

    Code review of hr_business_unit

--- hr_business_unit/__openerp__.py
+++ hr_business_unit/__openerp__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "HR Business Unit",
-    'summary': """HR Business Unit""",
-    'author': "ONESTEiN BV",
-    'website': "http://www.onestein.eu",
+    'name': 'HR Business Unit',
+    'summary': '''HR Business Unit''',
+    'author': 'ONESTEiN BV',
+    'website': 'http://www.onestein.eu',
     'category': 'Human Resources',
     'version': '9.0.1.0.0',
     'depends': [
@@ -16,8 +16,5 @@
         'security/ir.model.access.csv',
         'views/hr_employee.xml',
     ],
-    'demo': [],
     'installable': True,
-    'auto_install': False,
-    'application': False,
 }
--- hr_business_unit/models/hr_employee.py
+++ hr_business_unit/models/hr_employee.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields
@@ -8,4 +8,7 @@ from openerp import models, fields
 class hr_employee(models.Model):
     _inherit = "hr.employee"
 
-    business_unit_id = fields.Many2one('hr.business.unit', string="Business Unit")
+    business_unit_id = fields.Many2one(
+        'hr.business.unit',
+        string="Business Unit"
+    )
