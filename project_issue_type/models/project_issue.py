066e9c076fbfaa6e1e2334ab69fdb4f81258e779
Andrea <a.stirpe@onestein.nl>
2016-12-28 15:19:33 +0100

    Module project_issue_type ported to V9

---
+++ project_issue_type/models/project_issue.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class ProjectIssue(models.Model):
+
+    _inherit = 'project.issue'
+
+    issue_type_id = fields.Many2one(
+        'project.issue.type',
+        'Issue Type'
+    )
