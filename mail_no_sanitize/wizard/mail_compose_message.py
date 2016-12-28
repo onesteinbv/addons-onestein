b0c43d94faadec8d23c58e29682296b40ebe96f9
Andrea <a.stirpe@onestein.nl>
2016-12-28 16:09:37 +0100

    Module mail_no_sanitize ported to V9

---
+++ mail_no_sanitize/wizard/mail_compose_message.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class MailComposeMessage(models.TransientModel):
+    _inherit = 'mail.compose.message'
+
+    body = fields.Html(sanitize=False)
