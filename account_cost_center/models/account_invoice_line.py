05ed502e1adf4422dde37b126fefe2125adab91a
Andrea <a.stirpe@onestein.nl>
2017-01-02 10:58:48 +0100

    Add module account_cost_center

---
+++ account_cost_center/models/account_invoice_line.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    @api.model
+    def _default_cost_center(self):
+        return self.env['account.cost.center'].browse(
+            self._context.get('cost_center_id', None))
+
+    cost_center_id = fields.Many2one(
+        'account.cost.center', string='Cost Center',
+        default=_default_cost_center)
---
+++ account_cost_center/models/account_move_line.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    cost_center_id = fields.Many2one(
+        'account.cost.center',
+        string='Cost Center')
