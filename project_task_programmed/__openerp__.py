e971a876d04e6b608cf4c07b8c4f39545b87d5b4
Andrea <a.stirpe@onestein.nl>
2017-01-27 08:59:55 +0100

    Code review of project_task_programmed

--- project_task_programmed/__openerp__.py
+++ project_task_programmed/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Task Reminder Planner",
-    'summary': """Plan task reminders for any date field.""",
-    'author': "ONESTEiN BV",
+    'name': 'Task Reminder Planner',
+    'summary': 'Plan task reminders for any date field',
+    'author': 'ONESTEiN BV',
     'license': 'AGPL-3',
-    'website': "http://www.onestein.eu",
+    'website': 'http://www.onestein.eu',
     'images': ['static/description/main_screenshot.png'],
     'category': 'Custom',
     'version': '8.0.1.0.0',
--- project_task_programmed/models/project_task_alert.py
+++ project_task_programmed/models/project_task_alert.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from datetime import date, timedelta
@@ -42,70 +42,82 @@ class ProjectTaskAlert(models.Model):
              "is used. Example: %(name)s %(description)s"
     )
 
-    @api.model
-    def _merge_placeholders(self, incoming, rec):
-        values = rec.read()
-        try:
-            result = incoming % values
-        except:
-            result = incoming
-        return result
-
     @api.multi
     def create_task_alerts(self):
         for task_alert in self:
-            days_delta = timedelta(days=task_alert.days_delta)
-            to_date = str(date.today() + days_delta)
-            last_run = task_alert.last_run or fields.Date.today()
-            task_alert_date = task_alert.date_field_id.name
-            args = [
-                (task_alert_date, '!=', False),
-                (task_alert_date, '<=', to_date),
-                (task_alert_date, '>=', last_run),
-            ]
-            model_name = task_alert.date_field_id.model_id.model
-            rec_ids = self.env[model_name].search(args)
-            for rec in rec_ids:
-                # Check if the task has already been created
-                ref_date = rec[task_alert.date_field_id.name]
-                prev_tasks = self.env['project.task'].search([
-                    ('alert_res_id', '=', rec.id),
-                    ('alert_model_name', '=', rec._name),
-                    ('alert_field_name', '=', task_alert.date_field_id.name),
-                    ('alert_to_date', '<=', to_date),
-                ])
-                to_continue = False
-                for prev_task in prev_tasks:
-                    prev_to_date = prev_task.alert_to_date
-                    if prev_to_date >= to_date:
-                        to_continue = True
-                        break
-                    elif ref_date <= prev_to_date:
-                        to_continue = True
-                        break
-                if to_continue:
-                    continue
+            task_alert._create_task_alerts()
 
-                name = self._merge_placeholders(
-                    task_alert.name, rec)
-                description = ''
-                if task_alert.task_description:
-                    description = self._merge_placeholders(
-                        task_alert.task_description, rec)
-                userid = task_alert.user_id and task_alert.user_id.id or None
+    @api.multi
+    def _create_task_alerts(self):
+
+        def merge_placeholders(incoming, rec):
+            if incoming:
+                values = rec.read()
+                try:
+                    result = incoming % values
+                except:
+                    result = incoming
+                return result
+            return ''
+
+        def task_alert_to_create(prev_tasks, ref_date, to_date):
+            to_create = True
+            for prev_task in prev_tasks:
+                prev_to_date = prev_task.alert_to_date
+                if prev_to_date >= to_date:
+                    to_create = False
+                    break
+                elif ref_date <= prev_to_date:
+                    to_create = False
+                    break
+            return to_create
+
+        def get_last_run(last_run):
+            return last_run or fields.Date.today()
+
+        def get_user_id(user):
+            return user and user.id or None
+
+        self.ensure_one()
+
+        days_delta = timedelta(days=self.days_delta)
+        to_date = str(date.today() + days_delta)
+        last_run = get_last_run(self.last_run)
+        task_alert_date = self.date_field_id.name
+        args = [
+            (task_alert_date, '!=', False),
+            (task_alert_date, '<=', to_date),
+            (task_alert_date, '>=', last_run),
+        ]
+        model_name = self.date_field_id.model_id.model
+        rec_ids = self.env[model_name].search(args)
+        for rec in rec_ids:
+            # Check if the task has already been created
+            ref_date = rec[self.date_field_id.name]
+            prev_tasks = self.env['project.task'].search([
+                ('alert_res_id', '=', rec.id),
+                ('alert_model_name', '=', rec._name),
+                ('alert_field_name', '=', self.date_field_id.name),
+                ('alert_to_date', '<=', to_date),
+            ])
+
+            if task_alert_to_create(prev_tasks, ref_date, to_date):
+                name = merge_placeholders(self.name, rec)
+                description = merge_placeholders(self.task_description, rec)
+                user_id = get_user_id(self.user_id)
                 task_data = {
                     'name': name,
-                    'project_id': task_alert.project_id.id,
-                    'user_id': userid,
+                    'project_id': self.project_id.id,
+                    'user_id': user_id,
                     'description': description,
                     'alert_model_name': rec._name,
                     'alert_res_id': rec.id,
-                    'alert_field_name': task_alert.date_field_id.name,
+                    'alert_field_name': self.date_field_id.name,
                     'alert_to_date': to_date,
-                    'alert_origin_id': task_alert.id,
+                    'alert_origin_id': self.id,
                 }
                 self.env['project.task'].create(task_data)
-            task_alert.last_run = fields.Date.today()
+        self.last_run = fields.Date.today()
 
     @api.model
     def run_task_alerts(self):
