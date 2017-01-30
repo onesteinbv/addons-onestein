0312afdb5495a319d7ef35ba425771843120cad4
antonio <espo-tony@hotmail.it>
2017-01-30 12:32:29 +0100

    Code refactoring of module 'web_widget_float_highlight';

--- web_widget_float_highlight/static/src/js/web_highlight.js
+++ web_widget_float_highlight/static/src/js/web_highlight.js
@@ -4,6 +4,11 @@ odoo.define("web.float.highlight", function (require) {
 
     form_widgets.FieldFloat.include({
 
+
+        _load_my_default: function (option, parameter, load_defaults, load_value, def_value) {
+            return (option && parameter) || load_defaults && load_value || def_value;
+        },
+
         init: function () {
             this._super.apply(this, arguments);
 
@@ -24,18 +29,18 @@ odoo.define("web.float.highlight", function (require) {
             )
                 internal_apply = true;
 
-            var load_defaults = (this.options && this.options.load_defaults) || false;
+            var load_defaults = this._load_my_default(this.options, this.options.load_defaults, false, false, false);
 
             var options = {
-                lower_threshold: (this.options && this.options.lower_threshold) || 0,
-                upper_threshold: (this.options && this.options.upper_threshold) || 0,
-                lower_bg_color: (this.options && this.options.lower_bg_color) || load_defaults && "red" || "white",
-                middle_bg_color: (this.options && this.options.middle_bg_color) || "white",
-                upper_bg_color: (this.options && this.options.upper_bg_color) || load_defaults && "green" || "white",
-                lower_font_color: (this.options && this.options.lower_font_color) || load_defaults && "white" || "#666666",
-                middle_font_color: (this.options && this.options.middle_font_color) || "#666666",
-                upper_font_color: (this.options && this.options.upper_font_color) || load_defaults && "white" || "#666666",
-                always_work: (this.options && this.options.always_work) || false,
+                lower_threshold: this._load_my_default(this.options, this.options.lower_threshold, false, false, 0),
+                upper_threshold: this._load_my_default(this.options, this.options.upper_threshold, false, false, 0),
+                lower_bg_color: this._load_my_default(this.options, this.options.lower_bg_color, load_defaults, "red", "white"),
+                middle_bg_color: this._load_my_default(this.options, this.options.middle_bg_color, false, false, "white"),
+                upper_bg_color: this._load_my_default(this.options, this.options.upper_bg_color, load_defaults, "green", "white"),
+                lower_font_color: this._load_my_default(this.options, this.options.lower_font_color, load_defaults, "white", "#666666"),
+                middle_font_color: this._load_my_default(this.options, this.options.middle_font_color, false, false, "#666666"),
+                upper_font_color: this._load_my_default(this.options, this.options.upper_font_color, load_defaults, "white", "#666666"),
+                always_work: this._load_my_default(this.options, this.options.always_work, false, false, false),
                 load_defaults: load_defaults,
                 apply: internal_apply
             };
