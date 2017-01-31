03271b56ec6bc2c97577373ee16e318434a92b47
Andrea <a.stirpe@onestein.nl>
2017-01-31 16:50:29 +0100

    Code review of account_analytic_expected_hours_notify

--- account_analytic_expected_hours_notify/__openerp__.py
+++ account_analytic_expected_hours_notify/__openerp__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "Account Analytic Expected Hours Notify",
     'images': [],
     'summary': """Notify project manager on reaching expected hours""",
-    'author': "ONESTEiN BV",
+    'author': 'Onestein',
     'license': 'AGPL-3',
-    'website': "http://www.onestein.eu",
+    'website': 'http://www.onestein.eu',
     'category': 'Sales Management',
     'version': '8.0.1.0.0',
     'depends': [
