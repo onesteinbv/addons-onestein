43f995ee74d42aaf1a4048b8498820342627665c
Andrea <a.stirpe@onestein.nl>
2017-02-01 14:21:14 +0100

    Developing bi_view_editor

--- bi_view_editor/models/models.py
+++ bi_view_editor/models/models.py
@@ -13,3 +13,9 @@ class Base(models.AbstractModel):
         table = self._table[0:6]
         if table != 'x_bve_':
             super(Base, self)._auto_end()
+
+    @api.model
+    def _setup_complete(self):
+        name = self._name[0:6]
+        if name != 'x_bve.':
+            super(Base, self)._setup_complete()
