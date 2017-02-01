f1f340d9e44fd2a742d369ee239d3365ee9a64b7
Andrea <a.stirpe@onestein.nl>
2017-02-01 08:51:27 +0100

    Updated author of mass_mailing_no_unsubscribe

--- mass_mailing_no_unsubscribe/__manifest__.py
+++ mass_mailing_no_unsubscribe/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Mass mailing unsubscription',
     'images': ['static/description/main_screenshot.png'],
-    'summary': '''Configurable unsubscribe possibility for mass mailing''',
+    'summary': 'Configurable unsubscribe possibility for mass mailing',
     'version': '10.0.1.0.0',
-    'author': 'ONESTEiN BV',
+    'author': 'Onestein',
     'license': 'AGPL-3',
     'website': 'http://onestein.eu',
     'category': 'Marketing',
