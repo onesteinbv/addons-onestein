4f7866b52a1153eafdca9a4cfd1fd83b11aa6b4e
antonio <espo-tony@hotmail.it>
2017-02-02 16:29:56 +0100

    Code refactoring of module 'account_asset_maintenance'

--- account_asset_maintenance/__init__.py
+++ account_asset_maintenance/__init__.py
@@ -2,5 +2,5 @@
 # Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import models
-import wizard
+from . import models
+from . import wizard
