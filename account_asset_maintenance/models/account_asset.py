d5ddeaedaad32fe97906ad32e649c75467550f0c
antonio <espo-tony@hotmail.it>
2017-02-02 16:27:46 +0100

    Code refactoring of module 'account_asset_maintenance'

--- account_asset_maintenance/models/account_asset.py
+++ account_asset_maintenance/models/account_asset.py
@@ -11,12 +11,18 @@ class account_asset(models.Model):
     equipment_id = fields.Many2one('maintenance.equipment', string="Equipment")
 
     @api.model
+    def _check_internal_call(self, equipment):
+        if not self._context.get('internal_call', False) and equipment:
+            return True
+        return False
+
+    @api.model
     def create(self, values):
         context = self.env.context.copy()
         if context.get('default_type', False) == 'in_invoice':
             context.pop('default_type')
         res = super(account_asset, self.with_context(context)).create(values)
-        if not self._context.get('internal_call', False) and res.equipment_id:
+        if self._check_internal_call(res.equipment_id) :
             ctx = dict(context, internal_call=True)
             res.equipment_id.with_context(ctx).write({'asset_id': res.id})
         return res
@@ -25,12 +31,9 @@ class account_asset(models.Model):
     def write(self, values):
         for asset in self:
             ctx = dict(self.env.context, internal_call=True)
-            if not self._context.get('internal_call', False) and \
-                    asset.equipment_id:
+            if self._check_internal_call(asset.equipment_id):
                 asset.equipment_id.with_context(ctx).write({'asset_id': None})
             super(account_asset, asset).write(values)
-            if not self._context.get('internal_call', False) and \
-                    asset.equipment_id:
-                asset.equipment_id.with_context(ctx).write(
-                    {'asset_id': asset.id})
+            if self._check_internal_call(asset.equipment_id):
+                asset.equipment_id.with_context(ctx).write({'asset_id': asset.id})
         return True
