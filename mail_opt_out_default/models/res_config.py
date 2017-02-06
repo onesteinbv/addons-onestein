db75a2748d827cf1cea7c07bbadeaf0002047b45
Andrea <a.stirpe@onestein.nl>
2017-02-06 11:21:22 +0100

    Restored previous version of mail_opt_out_default

--- mail_opt_out_default/models/res_config.py
+++ mail_opt_out_default/models/res_config.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class BaseConfigSettings(models.TransientModel):
@@ -12,29 +12,8 @@ class BaseConfigSettings(models.TransientModel):
         default=lambda self: self.env.user.company_id.default_opt_out,
         string='Default Opt-out for partners *',
         store=True,
-        groups='base.group_system',
         help='''
         Set the Opt-out value to True by default
         for newly created Partners
         ''',
     )
-
-    @api.model
-    def create(self, vals):
-        res = super(BaseConfigSettings, self).create(vals)
-        if 'default_opt_out' in vals:
-            new_opt_out = vals.get('default_opt_out')
-            existing_opt_out = self.env.user.company_id.default_opt_out
-            if new_opt_out != existing_opt_out:
-                self.env.user.company_id.default_opt_out = new_opt_out
-        return res
-
-    @api.multi
-    def write(self, vals):
-        res = super(BaseConfigSettings, self).write(vals)
-        if 'default_opt_out' in vals:
-            new_opt_out = vals.get('default_opt_out')
-            existing_opt_out = self.env.user.company_id.default_opt_out
-            if new_opt_out != existing_opt_out:
-                self.env.user.company_id.default_opt_out = new_opt_out
-        return res
