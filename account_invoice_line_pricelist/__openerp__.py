1eb0400aeeeeb477662cb72fa246cdb126a4847b
Andrea <a.stirpe@onestein.nl>
2017-02-08 10:18:11 +0100

    Updated author of account_invoice_line_pricelist

--- account_invoice_line_pricelist/__openerp__.py
+++ account_invoice_line_pricelist/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -8,7 +8,7 @@
     'summary': """Prices on invoice products based on partner pricelists""",
     'version': '8.0.1.0.0',
     'category': 'Accounting & Finance',
-    'author': 'ONESTEiN BV',
+    'author': 'Onestein',
     'website': 'http://www.onestein.eu',
     'license': 'AGPL-3',
     'depends': [
