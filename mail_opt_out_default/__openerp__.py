e6de902cf51fc30f7a470299a81a3a7d97b10099
Andrea <a.stirpe@onestein.nl>
2017-02-08 10:24:51 +0100

    Updated author of mail* modules

--- mail_opt_out_default/__openerp__.py
+++ mail_opt_out_default/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -7,7 +7,7 @@
     'images': [],
     'summary': 'Set field Opt-Out as true by default while creating a partner',
     'license': 'AGPL-3',
-    'author': 'ONESTEiN BV',
+    'author': 'Onestein',
     'website': 'http://www.onestein.eu',
     'category': 'Marketing',
     'version': '8.0.1.0.0',
