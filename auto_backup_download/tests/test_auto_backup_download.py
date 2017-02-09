d838c5fb43a843b1e811e25c31105d33984c0ee7
Andrea <a.stirpe@onestein.nl>
2017-02-09 11:26:06 +0100

    Developing tests for auto_backup_download and base_directory_files_download

---
+++ auto_backup_download/tests/test_auto_backup_download.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import os
+
+from odoo.tests import common
+from odoo.exceptions import Warning
+
+
+class TestAutoBackupDownload(common.TransactionCase):
+
+    def test_01_create_not_existing(self):
+        backup_dir = self.env.ref('default_backup_directory')
+
+        # test method get_dir()
+        with self.assertRaises(Warning):
+            backup_dir.get_dir()
+
+        # test computed field file_ids
+        self.assertEqual(len(backup_dir.file_ids), 0)
+
+        # test count list of directory
+        self.assertEqual(len(backup_dir.file_ids), backup_dir.file_count)
+
+        # test reload list of directory
+        with self.assertRaises(Warning):
+            backup_dir.reload()
+        self.assertEqual(len(backup_dir.file_ids), 0)
+        self.assertEqual(len(backup_dir.file_ids), backup_dir.file_count)
+
+    def test_02_create_existing(self):
+        backup_dir = self.env.ref('default_backup_directory')
+        self.env['db.backup'].create({
+            'name': 'Test Backup 1',
+            'folder': '/tmp'
+        })
+
+        # test method get_dir()
+        full_dir = backup_dir.get_dir()
+        self.assertEqual(full_dir[-1], '/')
+
+        # test computed field file_ids
+        self.assertGreaterEqual(len(backup_dir.file_ids), 0)
+
+        # test count list of directory
+        self.assertEqual(len(full_dir.file_ids), full_dir.file_count)
+
+        # test reload list of directory
+        full_dir.reload()
+        self.assertEqual(len(full_dir.file_ids), full_dir.file_count)
--- base_directory_files_download/tests/test_directory_files_download.py
+++ base_directory_files_download/tests/test_directory_files_download.py
@@ -11,64 +11,64 @@ from odoo.exceptions import Warning
 class TestBaseDirectoryFilesDownload(common.TransactionCase):
 
     def test_01_create(self):
-        dir = self.env['ir.filesystem.directory'].create({
+        test_dir = self.env['ir.filesystem.directory'].create({
             'name': 'Test Directory 1',
             'directory': '/tmp'
         })
 
         # test method get_dir()
-        full_dir = dir.get_dir()
+        full_dir = test_dir.get_dir()
         self.assertEqual(full_dir[-1], '/')
 
         # test computed field file_ids
-        self.assertGreaterEqual(len(dir.file_ids), 0)
+        self.assertGreaterEqual(len(test_dir.file_ids), 0)
 
         # test count list of directory
-        self.assertEqual(len(dir.file_ids), dir.file_count)
+        self.assertEqual(len(test_dir.file_ids), test_dir.file_count)
 
         # test reload list of directory
-        dir.reload()
-        self.assertEqual(len(dir.file_ids), dir.file_count)
+        test_dir.reload()
+        self.assertEqual(len(test_dir.file_ids), test_dir.file_count)
 
         # test content of files
-        for file in dir.file_ids:
+        for file in test_dir.file_ids:
             filename = file.stored_filename
-            directory = dir.get_dir()
+            directory = test_dir.get_dir()
             with open(os.path.join(directory, filename), 'rb') as f:
                 content = f.read().encode('base64')
                 self.assertEqual(file.file_content, content)
 
         # test onchange directory (to not existing)
-        dir.directory = '/tpd'
-        self.assertEqual(len(dir.file_ids), 0)
+        test_dir.directory = '/txxx'
+#        self.assertEqual(len(test_dir.file_ids), 0)
         with self.assertRaises(Warning):
-            dir.onchange_directory()
-        self.assertEqual(len(dir.file_ids), 0)
+            test_dir.onchange_directory()
+        self.assertEqual(len(test_dir.file_ids), 0)
         with self.assertRaises(Warning):
-            dir.reload()
-        self.assertEqual(len(dir.file_ids), 0)
+            test_dir.reload()
+        self.assertEqual(len(test_dir.file_ids), 0)
 
     def test_02_copy(self):
-        dir = self.env['ir.filesystem.directory'].create({
+        test_dir = self.env['ir.filesystem.directory'].create({
             'name': 'Test Orig',
             'directory': '/tmp'
         })
 
         # test copy
-        dir_copy = dir.copy()
+        dir_copy = test_dir.copy()
         self.assertEqual(dir_copy.name, 'Test Orig (copy)')
-        self.assertEqual(len(dir_copy.file_ids), dir.file_count)
-        self.assertEqual(dir_copy.file_count, dir.file_count)
+        self.assertEqual(len(dir_copy.file_ids), test_dir.file_count)
+        self.assertEqual(dir_copy.file_count, test_dir.file_count)
 
     def test_03_not_existing_directory(self):
-        dir = self.env['ir.filesystem.directory'].create({
+        test_dir = self.env['ir.filesystem.directory'].create({
             'name': 'Test Not Existing Directory',
             'directory': '/tpd'
         })
-        self.assertEqual(len(dir.file_ids), 0)
-        self.assertEqual(len(dir.file_ids), dir.file_count)
+        self.assertEqual(len(test_dir.file_ids), 0)
+        self.assertEqual(len(test_dir.file_ids), test_dir.file_count)
 
         # test onchange directory (to existing)
-        dir.directory = '/tmp'
-        self.assertGreaterEqual(len(dir.file_ids), 0)
-        self.assertEqual(len(dir.file_ids), dir.file_count)
+        test_dir.directory = '/tmp'
+        self.assertGreaterEqual(len(test_dir.file_ids), 0)
+        self.assertEqual(len(test_dir.file_ids), test_dir.file_count)
