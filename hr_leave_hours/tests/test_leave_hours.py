d8a6d4144bc702f8f61aded5ed56d94e48843e7e
antonio <espo-tony@hotmail.it>
2017-02-10 16:55:57 +0100

    Unit tests for module hr_leave_hours

--- hr_leave_hours/tests/test_leave_hours.py
+++ hr_leave_hours/tests/test_leave_hours.py
@@ -285,3 +285,10 @@ class TestLeaveHours(common.TransactionCase):
             self.today_start,
             self.today_end
         )])
+
+    def test_06_compute_leaves_count(self):
+        employee_list = self.employee_1 + \
+                        self.employee_2 + \
+                        self.employee_3 + \
+                        self.employee_4
+        employee_list._compute_leaves_count()
