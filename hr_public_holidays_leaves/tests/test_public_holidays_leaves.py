33fba9194bd6b003c6052d4e3a20b9e042e3f808
antonio <espo-tony@hotmail.it>
2017-02-27 15:08:06 +0100

    Code refactoring of module 'hr_public_holidays_leaves'

--- hr_public_holidays_leaves/tests/test_public_holidays_leaves.py
+++ hr_public_holidays_leaves/tests/test_public_holidays_leaves.py
@@ -3,8 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.tests import common
-from datetime import datetime
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DF
 from odoo.exceptions import Warning
 
 
