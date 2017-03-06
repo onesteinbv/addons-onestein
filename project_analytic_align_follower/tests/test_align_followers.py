74b07841be535f8e6ad306b09fd0f3f101820952
antonio <espo-tony@hotmail.it>
2017-03-06 13:59:08 +0100

    Unit tests for module project_analytic_align_follower

---
+++ project_analytic_align_follower/tests/test_align_followers.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from datetime import date, timedelta
+from odoo.tests import common
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
+from odoo.exceptions import ValidationError
+
+
+class TestAlignFollowers(common.TransactionCase):
+
+    def setUp(self):
+        super(TestAlignFollowers, self).setUp()
+
+        AnalyticAccount = self.env['account.analytic.account']
+        Project = self.env['project.project']
+        Follower = self.env['mail.followers']
+
+        self.aa_1 = AnalyticAccount.create({
+            'name': 'Analytic Account 1',
+        })
+
+        self.proj_1 = Project.create({
+            'name': 'Project 1',
+            'analytic_account_id': self.aa_1.id,
+        })
+
+        self.foll_2 = Follower.create({
+            'res_model': 'account.analytic.account',
+            'res_id': self.aa_1.id,
+            'partner_id': self.env.user.partner_id.id,
+        })
+
+        self.foll_1 = Follower.create({
+            'res_model': 'project.project',
+            'res_id': self.proj_1.id,
+            'partner_id': self.env.user.partner_id.id,
+        })
+
+    def test_01_unlink_proj(self):
+        self.proj_1.unlink()
+
+    def test_02_unlink_aa(self):
+        self.aa_1.unlink()
+
+    def test_dummy(self):
+        None
