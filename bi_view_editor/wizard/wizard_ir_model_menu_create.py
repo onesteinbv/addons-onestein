9378b72789506d233e7d440e45c64fd1402c3b0f
Andrea <a.stirpe@onestein.nl>
2017-03-08 16:54:35 +0100

    Improving menu creation in bi_view_editor

--- bi_view_editor/wizard/wizard_ir_model_menu_create.py
+++ bi_view_editor/wizard/wizard_ir_model_menu_create.py
@@ -14,17 +14,10 @@ class WizardModelMenuCreate(models.TransientModel):
             self.ensure_one()
             active_id = self._context.get('active_id')
             bve_view = self.env['bve.view'].browse(active_id)
-            menu = self.env['ir.ui.menu'].create({
+            self.env['ir.ui.menu'].create({
                 'name': self.name,
                 'parent_id': self.menu_id.id,
                 'action': 'ir.actions.act_window,%d' % (bve_view.action_id,)
-            })
-            self.env['ir.model.data'].create({
-                'name': bve_view.name,
-                'noupdate': True,
-                'module': 'bi_view_editor',
-                'model': 'ir.ui.menu',
-                'res_id': menu.id,
             })
             return {'type': 'ir.actions.act_window_close'}
         return super(WizardModelMenuCreate, self).menu_create()
