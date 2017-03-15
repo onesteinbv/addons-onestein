ad993b6c6a712b050a2add616ed57b042fbecae4
antonio <espo-tony@hotmail.it>
2017-03-15 09:05:57 +0100

    Code refactoring of module hr_holidays_leave_repeated

--- hr_holidays_leave_repeated/models/hr_holidays.py
+++ hr_holidays_leave_repeated/models/hr_holidays.py
@@ -3,7 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from dateutil.relativedelta import relativedelta
-from datetime import datetime
 
 from odoo import api, fields, models
 from odoo.exceptions import UserError
