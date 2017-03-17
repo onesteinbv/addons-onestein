cc155e7df164602bfa93e696ca899742d6d5d867
antonio <espo-tony@hotmail.it>
2017-03-17 12:02:57 +0100

    Code refactoring, module 'account_cost_center'

--- account_cost_center/tests/test_cost_center.py
+++ account_cost_center/tests/test_cost_center.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestAccountCostCenter(common.TransactionCase):
