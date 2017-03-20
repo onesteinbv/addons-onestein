b07f4817ea1b09cffaf8c12c5e0dee956fadaf5a
Andrea <a.stirpe@onestein.nl>
2017-03-20 14:52:29 +0100

    Removed unused file

--- l10n_nl_tax_statement/models/l10n_nl_vat_statement_config_wizard.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from odoo import api, fields, models, _
-
-
-class VatStatementConfigWizard(models.TransientModel):
-    _name = 'l10n.nl.vat.statement.config.wizard'
