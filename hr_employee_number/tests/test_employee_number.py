32861ffecccafd40959dcc0dbfb86d9fbfccb55e
antonio <espo-tony@hotmail.it>
2017-03-24 16:45:54 +0100

    Unit tests for module hr_employee_number

---
+++ hr_employee_number/tests/test_employee_number.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestEmployeeNumber(TransactionCase):
+
+    def test_01(self):
+        employee = self.env['hr.employee'].create({
+            'name': 'Employee',
+            'employee_number': '-',
+        })
+
+        self.assertNotEqual(employee.employee_number, '1')
