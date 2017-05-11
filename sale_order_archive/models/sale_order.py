a9ebfcbbdc8afd6caacf9ae001aaf68e3c9d0b84
Andrea <a.stirpe@onestein.nl>
2017-05-11 15:44:10 +0200

    Removed unused import

--- sale_order_archive/models/sale_order.py
+++ sale_order_archive/models/sale_order.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
