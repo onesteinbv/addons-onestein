320e2501f25888b5247621dd98ae0894b026941d
Andrea <a.stirpe@onestein.nl>
2017-05-11 15:30:30 +0200

    Add sale_order_archive

---
+++ sale_order_archive/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Sale Order Archive',
+    'summary': 'Archive Sale Orders',
+    'author': 'Onestein',
+    'website': 'http://www.onestein.eu',
+    'category': 'Sales',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_order.xml',
+    ],
+    'installable': True,
+}
