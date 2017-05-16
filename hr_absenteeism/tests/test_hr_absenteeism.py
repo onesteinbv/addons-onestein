b18a8dab23e29c539094542c05b40e524dbfbb16
Andrea <a.stirpe@onestein.nl>
2017-05-16 10:01:43 +0200

    Update tests of hr_absenteeism* modules

--- hr_absenteeism/tests/test_hr_absenteeism.py
+++ hr_absenteeism/tests/test_hr_absenteeism.py
@@ -3,9 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from odoo.tests import common
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DF
-from dateutil.relativedelta import relativedelta
 from odoo.exceptions import Warning
 
 
@@ -18,16 +18,16 @@ class TestHRAbsenteeism(common.TransactionCase):
         self.Employee = self.env['hr.employee']
         self.Notification = self.env['hr.absenteeism.notifications']
 
-        self.today_start = datetime.today().replace(
-            hour=8, minute=0, second=0) - relativedelta(days=1)
-        self.today_end = datetime.today().replace(
-            hour=18, minute=0, second=0) - relativedelta(days=1)
+        self.start = (datetime.today() - relativedelta(days=1)).replace(
+            hour=8, minute=0, second=0)
+        self.end = (datetime.today() - relativedelta(days=1)).replace(
+            hour=18, minute=0, second=0)
 
-        today_start = self.today_start.strftime(DF)
-        today_end = self.today_end.strftime(DF)
+        start = self.start.strftime(DF)
+        end = self.end.strftime(DF)
 
-        self.l_start = (self.today_start + relativedelta(days=2)).strftime(DF)
-        self.l_end = (self.today_end + relativedelta(days=2)).strftime(DF)
+        self.l_start = (self.start + relativedelta(days=2)).strftime(DF)
+        self.l_end = (self.end + relativedelta(days=2)).strftime(DF)
 
         self.employee_1 = self.Employee.create({
             'name': 'Employee 1',
@@ -52,13 +52,13 @@ class TestHRAbsenteeism(common.TransactionCase):
             'holiday_status_id': self.status_1.id,
             'holiday_type': 'employee',
             'type': 'remove',
-            'date_from': today_start,
-            'date_to': today_end,
+            'date_from': start,
+            'date_to': end,
             'employee_id': self.employee_1.id,
         })
 
-    # def test_01_increase_date_to(self):
-    #     self.Holidays.increase_date_to()
+    def test_01_increase_date_to(self):
+        self.Holidays.increase_date_to()
 
     def test_02_increase_date_to(self):
 
