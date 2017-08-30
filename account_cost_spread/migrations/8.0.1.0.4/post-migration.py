3dace728de7e400cf951c13ed095acab3081594d
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-08-30 11:15:57 +0200

    Add test data and test structure

---
+++ account_cost_spread/migrations/8.0.1.0.4/post-migration.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import api, SUPERUSER_ID, tools
+
+
+def migrate(cr, version=None):
+    import pudb
+    pudb.set_trace()
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    account_inv_sl_model = env['account.invoice.spread.line']
+    for spreadline in account_inv_sl_model.search([]):
+        il_id = spreadline.invoice_line_id
+        # check if already reconciled
+        has_reconciliation = False
+        if len(spreadline.move_id.line_id.mapped('reconcile_id') +
+               spreadline.move_id.line_id.mapped('reconcile_partial_id')) > 0:
+            has_reconciliation = True
+        if (il_id.spread_account_id and 
+                il_id.spread_account_id.reconcile and
+                il_id.spread_account_id == il_id.account_id and
+                has_reconciliation == False):
+            spreadline.move_id.line_id.reconcile()
