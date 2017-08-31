003105b87aaec6a03a94dcd90d790ccdc8556c0f
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-08-31 19:07:34 +0200

    complete tests

--- account_cost_spread/tests/test_account_cost_spread.py
+++ account_cost_spread/tests/test_account_cost_spread.py
@@ -11,8 +11,6 @@ class TestAccountCostSpread(TransactionCase):
 
     def setUp(self):
         super(TestAccountCostSpread, self).setUp()
-        import pudb
-        pudb.set_trace()
         self.acc_inv_model = self.env['account.invoice']
         self.acc_inv_l_model = self.env['account.invoice.line']
         self.acc_inv_spread_l_model = self.env['account.invoice.spread.line']
@@ -31,6 +29,9 @@ class TestAccountCostSpread(TransactionCase):
         self.product_id = self.ref("product.product_product_4")
         self.bank_journal_usd_id = self.env.ref("account.bank_journal_usd")
         self.account_usd_id = self.env.ref("account.usd_bnk")
+
+
+    def test_all(self):
         # make product with account
         self.product_t = self.env['product.product'].create({
             'name':  'product_demo',
@@ -78,9 +79,69 @@ class TestAccountCostSpread(TransactionCase):
         self.assertEqual(len(self.invoice.invoice_line.spread_line_ids), 4)
         # create move for every spread
         moves = []
-        for spread in self.invoice.invoice_line.spread_line_ids:
-            moves += spread.create_move()
-        #check if reconciled
-        for move in moves:
-            self.assertEqual(bool(move.line_id.reconcile_id), True)
+        for spread_l in self.invoice.invoice_line.spread_line_ids:
+            moves += spread_l.create_move()
+        #account cannot be reconciled
+        for move in self.env['account.move'].browse(moves):
+            for line in move.line_id:
+                self.assertEqual(bool(line.reconcile_id), False)
         #check if you can delete
+        for spread_l in self.invoice.invoice_line.spread_line_ids:
+            previous_move = spread_l.move_id.ids
+            spread_l.unlink_move()
+            # exists?
+            self.assertEqual(spread_l.move_id.ids, [])
+            self.assertEqual(self.env['account.move'].search(
+                [('id', 'in', previous_move)]
+            )
+        # make account_fx_income_id reconcilable
+        self.account_fx_income_id.write({'reconcile':True})
+        # make an invoice with the new reconciliable account
+        self.invoice_rec = self.acc_inv_model.create({
+            'account_id': self.account_fx_income_id.id,
+            'company_id': self.partner_agrolait_id.company_id.id,
+            'currency_id': self.currency_usd_id.id,
+            'invoice_line': [(
+                0, 0, {'account_id': self.account_fx_income_id.id,
+                       'name': 'line1' ,
+                       'product_id': self.product_t.id,
+                       'product_uom_qty': 2000,
+                       'uos_id': self.product_t.uos_id.id,
+                       'price_unit': 33}
+            )],
+            'journal_id': self.bank_journal_usd_id.id ,
+            'partner_id':  self.partner_agrolait_id.id,
+            'reference_type': 'none'
+        })
+        # confirm
+        self.invoice_rec.action_date_assign()
+        self.invoice_rec.action_move_create()
+        self.invoice_rec.action_number()
+        self.invoice_rec.invoice_validate()
+        self.assertEqual(
+            self.invoice_rec.date_invoice, date.today().strftime(
+                DEFAULT_SERVER_DATE_FORMAT
+            )
+        )
+        # Make a spread on the line for the next 4 months
+        # TODO a period must exist, verify that is allways the case
+        self.invoice_rec.invoice_line[0].write(
+            {'spread_account_id': self.account_fx_income_id.id,
+             'period_number': 4,
+             'period_type': 'month',
+            }
+        )
+        self.invoice_rec.invoice_line[0].action_recalculate_spread()
+        self.assertEqual(len(self.invoice_rec.invoice_line.spread_line_ids), 4)
+        moves = []
+        for spread in self.invoice_rec.invoice_line.spread_line_ids:
+            moves += spread.create_move()
+        #account cannot be reconciled
+        for move in self.env['account.move'].browse(moves):
+            for line in move.line_id:
+                self.assertEqual(bool(line.reconcile_id), True)
+        for spread_l in self.invoice.invoice_line.spread_line_ids:
+            spread_l.unlink_move()
+            self.assertEqual(spread_l.move_id.ids, [])
+        # Check if the connected moves still exist
+
