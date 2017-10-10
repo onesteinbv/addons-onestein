df208b0dc361b6ef2a5b1ea8b3f0b1a03cc7bf55
luca-vercelli <luca.vercelli.to@gmail.com>
2017-10-10 15:49:56 +0200

    override write() and create()

--- account_invoice_line_pricelist/models/account_invoice_line.py
+++ account_invoice_line_pricelist/models/account_invoice_line.py
@@ -12,17 +12,28 @@ class AccountInvoiceLine(models.Model):
 
     @api.onchange('product_id')
     def _onchange_product_id(self):
+
         res = super(AccountInvoiceLine, self)._onchange_product_id()
 
+        price_unit = self._common_recalc_price()
+        if price_unit is not None and price_unit != 0:
+            self.price_unit = price_unit
+        
+        return res
+
+    def _common_recalc_price(self):
+        """
+        Re/calculate and return given price
+        """
         if self.invoice_id.type not in ['out_invoice', 'out_refund']:
-            return res
+            return None
 
         partner = self.invoice_id.partner_id
         pricelist = self.invoice_id.pricelist_id
         product = self.product_id
 
         if not partner or not product or not pricelist:
-            return res
+            return None
 
         inv_date = self.invoice_id.date_invoice or date.today().strftime(DT)
         product = product.with_context(
@@ -34,9 +45,42 @@ class AccountInvoiceLine(models.Model):
             uom=self.uom_id.id
         )
 
-        self.price_unit = self.env['account.tax']._fix_tax_included_price(
+        return self.env['account.tax']._fix_tax_included_price(
             product.price,
             product.taxes_id,
             self.invoice_line_tax_ids
         )
-        return res
+        
+    def create(self, vals):
+        """
+        Update price if missing, after real create()
+        """
+        line = super(AccountInvoiceLine, self).create(vals)
+        if line.invoice_id:
+            invoice = line.invoice_id
+            if ((not line.price_unit) or price_unit == 0) and (invoice.pricelist_id and invoice.pricelist_id.id):
+                price_unit = line._common_recalc_price()
+                if price_unit is not None and price_unit != 0:
+                    line.price_unit = price_unit    #triggers write()
+        return line
+    
+    @api.multi
+    def write(self, vals):
+        """
+        Update price whenever missing, before real write()
+        Remember: do *not* update rec.price_unit directly! that triggers write()
+        """
+        for rec in self:
+            if (rec.invoice_id.pricelist_id and rec.invoice_id.pricelist_id.id):
+                if 'price_unit' in vals and vals['price_unit'] is None or vals['price_unit'] == 0:
+                    #INSERT/UPDATE price_unit
+                    price_unit = rec._common_recalc_price()
+                    if price_unit is not None:
+                        vals['price_unit'] = price_unit
+                else:
+                    #UPDATE record
+                    if rec.price_unit is None or rec.price_unit == 0:
+                        price_unit = rec._common_recalc_price()
+                        if price_unit is not None and price_unit != 0:
+                            vals['price_unit'] = price_unit
+        return super(AccountInvoiceLine, self).write(vals)
