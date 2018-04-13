4bfd292d864b8a01f5fb5a8eabc0ff99e09823f7
antonio <espo-tony@hotmail.it>
2018-04-13 16:02:06 +0200

    [11.0][ADD] Module sale_order_discount_formula

---
+++ sale_order_discount_formula/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Sale Order Multiple Discount",
+    'summary': "Express discounts on SO lines as mathematical expressions",
+    'author': 'Onestein, Odoo Community Association (OCA)',
+    'website': 'http://www.onestein.eu',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'version': '11.0.1.0.0',
+    'depends': [
+        'account_invoice_discount_formula',
+        'sale'
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+    ]
+}
