2576b7e1257e2a4369218e1541b032951f83f1ea
Andrea <a.stirpe@onestein.nl>
2018-07-11 13:52:53 +0200

    Code review of account_cost_spread_all

--- account_cost_spread_all/models/account_invoice_line.py
+++ account_cost_spread_all/models/account_invoice_line.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2016-2018 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
@@ -9,7 +8,7 @@ class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
     @api.multi
-    def _display_create_all_moves(self):
+    def _compute_display_create_all_moves(self):
         for line in self:
             line.display_create_all_moves = False
             for spread in line.spread_line_ids:
@@ -18,7 +17,7 @@ class AccountInvoiceLine(models.Model):
                     break
 
     display_create_all_moves = fields.Boolean(
-        compute='_display_create_all_moves',
+        compute='_compute_display_create_all_moves',
         string='Display Button All Moves')
 
     @api.multi
