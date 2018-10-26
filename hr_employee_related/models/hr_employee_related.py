5d097dacf46134e9d0fde802b88ea51344ab9cb4
Cas Vissers <c.vissers@brahoo.nl>
2018-10-26 13:54:23 +0200

    [IMP] make relation selection field (#47)

--- hr_employee_related/models/hr_employee_related.py
+++ hr_employee_related/models/hr_employee_related.py
@@ -18,7 +18,20 @@ class HrEmployeeRelated(models.Model):
          ('female', 'Female')],
         string='Gender'
     )
-    relation = fields.Char('Employee relation')
+    relation = fields.Selection(
+        [('spouse', 'Spouse'),
+         ('partner', 'Partner'),
+         ('son', 'Son'),
+         ('daughter', 'Daughter'),
+         ('father', 'Father'),
+         ('mother', 'Mother'),
+         ('brother', 'Brother'),
+         ('sister', 'Sister'),
+         ('caretaker', 'Caretaker'),
+         ('other', 'Other'),
+         ],
+        string='Employee relation',
+        default='partner')
     bsn_number = fields.Char('BSN')
     birth_date = fields.Date('Birth date')
     telephone = fields.Char('Telephone')
